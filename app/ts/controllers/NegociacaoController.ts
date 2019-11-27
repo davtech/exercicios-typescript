372c4b65ab3d1ce199487c467314fae5f2dd48ab
David Bastos <davtechh@gmail.com>
2019-11-27 10:46:54 -0300

    estruturando com enum e strict

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,7 +1,5 @@
-import { NegociacoesView } from '../views/NegociacoesView';
-import { MensagemView } from '../views/MensagemView';
-import { Negociacoes } from '../models/Negociacoes';
-import { Negociacao } from '../models/Negociacao';
+import { NegociacoesView, MensagemView } from '../views/index';
+import { Negociacoes, Negociacao } from '../models/index';
 
 export class NegociacaoController {
 
@@ -24,6 +22,12 @@ export class NegociacaoController {
 
     event.preventDefault();
 
+    if(!this._ehDiaUtil(data)) {
+
+      this._mensagemView.update('Somente negociações em dias úteis, por favor!');
+      return 
+    }
+
     const negociacao = new Negociacao(
       new Date(this._inputData.val().replace(/-/g, ',')),
       parseInt(this._inputQuantidade.val()),
@@ -34,4 +38,18 @@ export class NegociacaoController {
     this._negociacoesView.update(this._negociacoes);
     this._mensagemView.update('Negociação adicionada com sucesso');
   }
+  
+  private _ehDiaUtil(data: Date) {
+    return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
+  }
+}
+
+enum DiaDaSemana {
+  Domingo, 
+  Segunda, 
+  Terca, 
+  Quarta, 
+  Quinta, 
+  Sexta, 
+  Sabado
 }
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,12 +1,17 @@
 export abstract class View<T> {
   private _elemento: JQuery;
+  private _escapar: Boolean;
 
-  constructor(seletor: string) {
+  constructor(seletor: string, escapar: boolean = false) {
     this._elemento = $(seletor);
   }
 
   update(model: T): void {
-    this._elemento.html(this.template(model));
+    let template = this.template(model);
+    if(this._escapar) 
+      template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+
+    this._elemento.html(template);
   }
 
   abstract template(model: T): string;
